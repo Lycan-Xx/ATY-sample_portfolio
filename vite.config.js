5f8b842ccd2f6bed099a9edc58c859ce9f87caff
Lycan-Xx <msbello514@gmail.com>
2025-02-18 17:50:18 +0100

    Removed the public directory because it is not useful, added the base url to fix 'mime' errors

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  }
+	plugins: [react()],
+	optimizeDeps: {
+		exclude: ['lucide-react'],
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx']
+	},
+	base: '/ATY-sample_portfolio/',
 });
